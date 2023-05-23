f396d6c5a7b83afdc4c2f56f3a2e74876be9bc4f
Ayushmaan Sharma <106451252+ayushhh231@users.noreply.github.com>
2023-05-23 20:02:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Change the color of whole body
+function click1(){
+    document.body.style.backgroundColor = "red";
+}
+function click2(){
+    document.body.style.backgroundColor = "yellow";
+}
+function click3(){
+    document.body.style.backgroundColor = "green";
+}
+
+// Change the color of Div
+// function click1(){
+//     document.getElementById("theme").style.backgroundColor = "red";
+// }
+// function click2(){
+//     document.getElementById("theme").style.backgroundColor = "green";
+// }
+// function click3(){
+//     document.getElementById("theme").style.backgroundColor = "yellow";
+// }
